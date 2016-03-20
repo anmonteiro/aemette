d1291bfc737e8c8873089b05aae8468620663b99
António Nuno Monteiro <anmonteiro@gmail.com>
2016-03-20 17:11:16 +0100

    bump deps

--- src/leiningen/new/aemette/project.clj
+++ src/leiningen/new/aemette/project.clj
@@ -2,12 +2,12 @@
   :description "FIXME: write this!"
   :url "http://example.com/FIXME"
   :dependencies [[org.clojure/clojure "1.8.0"]
-                 [org.clojure/clojurescript "1.7.228"]
-                 [org.omcljs/om {{#om-legacy}}"0.9.0"{{/om-legacy}}{{^om-legacy}}"1.0.0-alpha30"{{/om-legacy}}]
+                 [org.clojure/clojurescript "1.8.34"]
+                 [org.omcljs/om {{#om-legacy}}"0.9.0"{{/om-legacy}}{{^om-legacy}}"1.0.0-alpha31"{{/om-legacy}}]
                  [org.clojure/core.async "0.2.374"]
                  [secretary "1.2.3"]]
 
-  :plugins [[lein-cljsbuild "1.1.2"]
+  :plugins [[lein-cljsbuild "1.1.3"]
             [lein-figwheel "0.5.0-6"]]
 
   :source-paths ["src"]
